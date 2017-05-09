0dc04b63cd9c2300ae3849924c305e4c8c10db4a
mmayoras <mkm7874@gmail.com>
2017-05-09 13:11:26 -0400

    Integrated webpack into application

--- src/main/java/com/homdepot/sa/cb/controller/HomeController.java
+++ src/main/java/com/homdepot/sa/cb/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.homdepot.sa.cb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by MXM6930 on 5/5/2017.
  */
-@RestController
+@Controller
 public class HomeController
 {
     @RequestMapping(value = "/")
