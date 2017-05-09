ddf4e69abef71ee8c5dbfded837c4ba9273b5978
mmayoras <mkm7874@gmail.com>
2017-05-09 13:14:59 -0400

    Adding missing files from last commit

---
+++ src/app.js
@@ -0,0 +1,1 @@
+import Hello from './main/webapp/hello.jsx';
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/src/main/webapp/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
